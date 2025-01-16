b02a5606c03f4da17763af44d7e62a6b1f32ab26
Kohantika Nath <kohan.nath@gmail.com>
2025-01-16 05:31:56 +0530

    BottomTabs pt2

--- src/navigation/BottomTabs.js
+++ src/navigation/BottomTabs.js
@@ -1,6 +1,25 @@
+// import React from "react";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { NavigationContainer } from "@react-navigation/native";
+// import TodoListScreen from "../screens/TodoListScreen";
+// import CompletedTasksScreen from "../screens/CompletedTasksScreen";
+
+// const Tab = createBottomTabNavigator();
+
+// export default function BottomTabs() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator>
+//         <Tab.Screen name="Todo List" component={TodoListScreen} />
+//         <Tab.Screen name="Completed Tasks" component={CompletedTasksScreen} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import TodoListScreen from "../screens/TodoListScreen";
 import CompletedTasksScreen from "../screens/CompletedTasksScreen";
 
@@ -9,10 +28,34 @@ const Tab = createBottomTabNavigator();
 export default function BottomTabs() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Todo List") {
+              iconName = "format-list-bulleted";
+            } else if (route.name === "Completed Tasks") {
+              iconName = "check";
+            }
+
+            return <Icon name={iconName} color={color} size={size} />;
+          },
+          headerTitleAlign: "center",
+        })}
+        tabBarOptions={{
+          activeTintColor: "#6200ee",
+          inactiveTintColor: "gray",
+          style: {
+            backgroundColor: "#f8f8f8",
+            paddingBottom: 5,
+          },
+        }}
+      >
         <Tab.Screen name="Todo List" component={TodoListScreen} />
         <Tab.Screen name="Completed Tasks" component={CompletedTasksScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
+
